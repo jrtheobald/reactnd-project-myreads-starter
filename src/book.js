19759bc132de59a0a480145dbd8bf21f5526b437
James R Theobald <jrtheobald_git@icloud.com>
2018-10-01 19:22:43 -0400

    Modify book.js, wrap comp in <li>

--- src/book.js
+++ src/book.js
@@ -6,6 +6,7 @@ class Book extends React.Component {
   }
     render() {
         return (
+          <li>
             <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
@@ -22,6 +23,7 @@ class Book extends React.Component {
                           <div className="book-title">{this.props.book.title}</div>
                           <div className="book-authors">{this.props.book.authors}</div>
                         </div>
+          </li>              
         );
     }
 }
